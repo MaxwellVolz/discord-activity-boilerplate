69b6cdfdfa4b6f514be8232e486c99c4be41e9b7
maxwellvolz <maxwellvolz@gmail.com>
2025-02-04 15:39:42 -0800

    third person camera and movement with maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { KeyboardControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Scene from "./components/Scene";
+import { Scene } from "./components/Scene";
+
+const keyboardMap = [
+  { name: "forward", keys: ["ArrowUp", "KeyW"] },
+  { name: "backward", keys: ["ArrowDown", "KeyS"] },
+  { name: "left", keys: ["ArrowLeft", "KeyA"] },
+  { name: "right", keys: ["ArrowRight", "KeyD"] },
+  { name: "run", keys: ["Shift"] },
+];
 
 function App() {
-
   return (
-    <Canvas>
-      <Scene />
-    </Canvas>
-  )
+    <KeyboardControls map={keyboardMap}>
+      <Canvas
+        shadows
+        camera={{ position: [3, 3, 3], near: 0.1, fov: 40 }}
+        style={{
+          touchAction: "none",
+        }}
+      >
+        <color attach="background" args={["#ececec"]} />
+        <Scene />
+      </Canvas>
+    </KeyboardControls>
+  );
 }
 
-export default App
+export default App;
